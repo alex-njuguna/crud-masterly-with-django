eae4f211ecb1fee2d41588caadaf6f42d7c96fe0
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-13 01:41:16 +0300

    crispy form

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -27,7 +27,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'webapp',
+
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
