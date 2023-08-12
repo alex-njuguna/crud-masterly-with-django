4bc5db83238c1652538aa9d09a2c92883a495817
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-12 21:05:09 +0300

    registration functionality

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import LoginForm, RegistrationForm
-
-
-
 
 
 def index(request):
@@ -11,6 +8,24 @@ def index(request):
     return render(request, 'webapp/index.html')
 
 
+def register(request):
+    """create a new user"""
+    form = RegistrationForm()
+    
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            # return redirect('')
+    
+    context = {
+        'form':form
+    }
+    
+    return render(request, 'webapp/register.html', context)
 
 
-
+def login(request):
+    """log in a user"""
+    pass
