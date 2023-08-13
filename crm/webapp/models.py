2e41216e79e0fd7e17b2f37e8359994b404e9fe7
alex-njuguna <njugunakinuthia013@gmail.com>
2023-08-13 19:10:53 +0300

    Records database formulation

--- crm/webapp/models.py
+++ crm/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Record(models.Model):
+    """table to store records"""
+    creation_date = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.BigIntegerField()
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
